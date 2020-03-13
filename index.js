db41a5890c03de5569c616a22f8bb4bf143e0e85
Hanna Lee <mani21na@gmail.com>
2020-03-13 16:11:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const testVar = {}
 
-function testFunc() {
-  return "hi"
+
+superbowlWin = (array) => {
+  let result = array.find(array => array.result === "W")
+  return result ? result.year : undefined
 }
